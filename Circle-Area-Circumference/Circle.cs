9e6e5cf297eaf14a1a5f710c8aa3d6b3e3492fc8
Eric Holt <fatmoti@gmail.com>
2019-05-01 15:21:02 -0400

    Initial Commit

---
+++ Circle-Area-Circumference/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area_Circumference
+{
+    public class Circle
+    {
+        public double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+            //Circumference = 2 × π × Radius
+            return Math.Round((2 * Math.PI) * radius,2); 
+        }
+        public double GetArea()
+        {
+            //The Area = π r2
+            return Math.Round(Math.PI * Math.Pow(radius, 2),2);
+        }
+
+        public void PrintInfo()
+        {
+            //using the other methods this should print the following in this order:
+            //The Radius
+            //The Area
+            //The Circumference
+            //Circumference: 18.85
+            //Area: 28.27
+            Console.WriteLine("Radius: "+ radius);
+            Console.WriteLine("Circumference: " + GetCircumference());
+            Console.WriteLine("Area: " + GetArea());
+        }
+    }
+}
---
+++ Circle-Area-Circumference/CircleApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area_Circumference
+{
+    class CircleApp
+    {
+        public List<Circle> Circles = new List<Circle>();
+
+        public void Run()
+        {
+            //bool isValidUserInput = false;
+            //double userInput = 0;
+            bool doAgain = true;
+            while (doAgain)
+            {
+                Console.WriteLine("Please enter the radius of a circle: ");
+                bool isValidUserInput = Double.TryParse(Console.ReadLine(), out double userInput);
+                if (isValidUserInput)
+                {
+                    Circle c = new Circle(userInput);
+                    Circles.Add(c);
+                }
+                else
+                {
+                    continue;
+                }
+                bool askToEnterAnotherCircle = true;
+                while (askToEnterAnotherCircle)
+                {
+                    Console.WriteLine("Would you like to add another circle? (yes or no)");
+                    string userInput2 = Console.ReadLine();
+                    if(userInput2.ToLower() == "yes" || userInput2.ToLower() == "y")
+                    {
+                        askToEnterAnotherCircle = false;
+                    }
+                    else if(userInput2.ToLower() == "no" || userInput2.ToLower() == "n")
+                    {
+                        askToEnterAnotherCircle = false;
+                        doAgain = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid response please enter yes or no");
+                    }
+                }
+
+            }
+            PrintCircles();
+        }
+        
+        public void PrintCircles()
+        {
+            foreach (Circle circle in Circles)
+            {
+                circle.PrintInfo();
+            }
+        }
+    }
+}
